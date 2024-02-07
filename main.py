c5cd746ed32d5ae248a40c1cb7390ee7df398300
Ninoyncrea <136315916+Ninoyncrea@users.noreply.github.com>
2024-02-07 13:29:36 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from src.serfveurtest2 import Server
 
 if __name__ == "__main__":
-    SERVER_HOST = '127.0.0.1'
+    SERVER_HOST = open("src/Master_ip.txt", "r").read().strip()
     SERVER_PORT = 5555
     server = Server(SERVER_HOST, SERVER_PORT)
     server.start()
